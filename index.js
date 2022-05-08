55e185c2e6d0573b0360764564d1bce4bfe09481
unknown <technicalsalma@gmail.com>
2022-05-08 23:20:20 +0600

    cors add

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const app = express();
 
 //middleware
 // app.use(cors());
-const corsOptions = {
-  origin: "https://sweet-fruit-house.web.app",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: "https://sweet-fruit-house.web.app",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
+// app.use(cors(corsOptions));
 // https://sweet-fruit-house.web.app
 app.use(express.json());
 
