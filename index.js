285937c743c8ce9adcd65287814100ad4c9abf8c
unknown <technicalsalma@gmail.com>
2022-05-08 23:06:01 +0600

    error solve

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 //middleware
-app.use(cors());
+// app.use(cors());
+const corsOptions = {
+  origin: "https://sweet-fruit-house.web.app",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+// https://sweet-fruit-house.web.app
 app.use(express.json());
 
 
