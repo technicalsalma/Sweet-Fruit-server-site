f1ecefd39c7b0ff7a68b7779356280a005ebe824
unknown <technicalsalma@gmail.com>
2022-05-14 11:19:42 +0600

    heroku problem

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken')
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-require('dotenv').config();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 
 //middleware
-// app.use(cors());
+const corsConfig = {
+  origin: true,
+  credentials: true,
+};
+app.use(cors(corsConfig));
+app.options("*", cors(corsConfig));
+
 // const corsOptions = {
 //   origin: "https://sweet-fruit-house.web.app",
 //   credentials: true, //access-control-allow-credentials:true
@@ -15,22 +21,22 @@ const app = express();
 // };
 // app.use(cors(corsOptions));
 // https://sweet-fruit-house.web.app
+
 app.use(express.json());
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eyw60.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-  try{
+async function run() {
+  try {
     await client.connect();
     const fruitCollection = client.db("SweetFruit").collection("fruitService");
- 
 
-    
-   //Fruit Api
+    //Fruit Api
     app.get("/fruitService", async (req, res) => {
       const query = {};
       const cursor = fruitCollection.find(query);
@@ -47,16 +53,20 @@ async function run(){
 
     app.post("/fruitService", async (req, res) => {
       const newInventory = req.body;
-  
+
       const result = await fruitCollection.insertOne(newInventory);
       res.send(result);
     });
 
     app.post("/login", async (req, res) => {
       const user = req.body;
-      const accessToken =await jwt.sign(user, process.env.ACCESS_TOTKEN_SECRE, {
-        expiresIn: "1d",
-      });
+      const accessToken = await jwt.sign(
+        user,
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+          expiresIn: "1d",
+        }
+      );
       res.send({ accessToken });
     });
 
@@ -91,39 +101,31 @@ async function run(){
       const tokenInfo = req.headers.authorization;
       const [email, accessToken] = tokenInfo.split(" ");
       const decoded = verifyToken(accessToken);
-      
+
       if (email === decoded.email) {
         const product = await fruitCollection.find({ email: email }).toArray();
         res.send(product);
       } else {
         res.send({ success: "UnAuthoraized Access" });
       }
-
-
     });
-
-  
-  }
-  finally{
-
+  } finally {
   }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
-
-
-app.get('/', (req , res)=>{
-    res.send('json is running');
+app.get("/", (req, res) => {
+  res.send("json is running");
 });
 
-app.listen(port,()=>{
-    console.log('john is running on port', port);
-})
+app.listen(port, () => {
+  console.log("john is running on port", port);
+});
 
 // verify token function =======>>>
 function verifyToken(token) {
   let email;
-  jwt.verify(token, process.env.ACCESS_TOTKEN_SECRE, function (err, decoded) {
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
     if (err) {
       email = "Invalid email";
     }
